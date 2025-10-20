1818ca72a27db27de6e9281ffd009251e48e6c77
Dmitriy Novikov <git.user.ndmitr5@gmail.com>
2025-10-21 00:43:45 +0300

    Улучшили doSomething2

--- src/main/java/com/stepup/proj1/MainApplication.java
+++ src/main/java/com/stepup/proj1/MainApplication.java
@@ -5,6 +5,7 @@ public class MainApplication {
         System.out.println("Hello World! 2");
         System.out.println("Hello World! 3");
         System.out.println("Hello World! 4");
+        doSomething();
         System.out.println("Hello World! 5");
     }
 
